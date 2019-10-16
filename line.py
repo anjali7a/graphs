4b0293cc481e5d88a357b19a9b7fcbcd69885a5a
anjali7a <56523436+anjali7a@users.noreply.github.com>
2019-10-15 21:27:00 -0700

    zigzag

--- line.py
+++ line.py
@@ -1,7 +1,8 @@
 from bokeh.plotting import figure, output_file, show
-x = (1, 2, 3, 4, 5)
-y = (6, 7, 2, 4, 5)
+x = (1, 2, 3, 4, 5, 6, 7, 8, 9)
+y = (175, 5, 190, 5, 205, 5, 200, 5, 199)
 output_file( "lines.html" )
 p = figure(title="simple line example", x_axis_label='y')
 p.line(x, y, legend="temp.", line_width=2)
 show(p)
+
